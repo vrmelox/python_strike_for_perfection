630010cd8c547022f0e4314bdfbb7e2f30efe484
vrmelox <akandeabiodoun@gmail.com>
2025-01-03 16:59:42 +0100

    i've added some features to master the basis of tkinter

--- tkinter_special/interface.py
+++ tkinter_special/interface.py
@@ -1,9 +1,21 @@
 from tkinter import *
+import time
+
+text = input("Ce que tu veux afficher en 8 secondes: ")
+time.sleep(8)
 root=Tk()
 root.title("Bienvenue sur ma fenêtre")
-root.geometry("800x800+0+0")
-root.resizable(False, False)
-root.config(bg="green")
+root.minsize(700, 600)
+root.maxsize(1200, 720)
+
+#root.geometry("800x800+0+0")
+#root.resizable(False, False)
+root.positionfrom("user")
+root.config(bg="hotpink")
+
+#les positions--------------Grid------------#
+
+lbl = Label(root, text=text).pack()
 
 
 root.mainloop()
