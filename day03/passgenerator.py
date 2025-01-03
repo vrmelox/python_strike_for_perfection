784c4f4ffaa8709b149e07e74a24b2ef0ef49188
vrmelox <akandeabiodoun@gmail.com>
2025-01-04 00:32:48 +0100

    i've managed the error case of the size. The password should contain at least 8 characters

--- day03/passgenerator.py
+++ day03/passgenerator.py
@@ -8,13 +8,15 @@ charsAll = the_lowercase + the_uppercase + the_number + the_symbol
 
 def genPass ():
     size = int(input("Je vous prie d'entrer la taille que vous souhaitez pour votre mot de passe (au moins 8): "))
-    password = ""
-    password += the_uppercase[random.randint(0, len(the_uppercase)) - 1]
-    password += the_lowercase[random.randint(0, len(the_lowercase)) - 1]
-    password += the_number[random.randint(0, len(the_number)) - 1]
-    password += the_symbol[random.randint(0, len(the_symbol)) - 1]
-    while (len(password) < size):
-        password += charsAll[random.randint(0, len(charsAll)) - 1]
-    print(password)
-
+    if size >= 8:
+        password = ""
+        password += the_uppercase[random.randint(0, len(the_uppercase)) - 1]
+        password += the_lowercase[random.randint(0, len(the_lowercase)) - 1]
+        password += the_number[random.randint(0, len(the_number)) - 1]
+        password += the_symbol[random.randint(0, len(the_symbol)) - 1]
+        while (len(password) < size):
+            password += charsAll[random.randint(0, len(charsAll)) - 1]
+        print(password)
+    else:
+        print("Erreur: au moins un mot de passe de taille 8")
 genPass()
