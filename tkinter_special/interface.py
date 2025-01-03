95fe130fbafbbf2d36e26ca5cb8fb92aa51ee8c7
vrmelox <akandeabiodoun@gmail.com>
2025-01-03 17:13:52 +0100

    I just used and widget to get and input from the user directly on the window

--- tkinter_special/interface.py
+++ tkinter_special/interface.py
@@ -1,8 +1,8 @@
 from tkinter import *
 import time
 
-text = input("Ce que tu veux afficher en 8 secondes: ")
-time.sleep(8)
+text = """Au commencement Dieu crea. Je ne dois pas etre en retard, sinon ce ne serait pas bien
+mais je dois essayer de voir ce qu'il ne pas pas."""
 root=Tk()
 root.title("Bienvenue sur ma fenêtre")
 root.minsize(700, 600)
@@ -10,12 +10,14 @@ root.maxsize(1200, 720)
 
 #root.geometry("800x800+0+0")
 #root.resizable(False, False)
-root.positionfrom("user")
+#root.positionfrom("user")
 root.config(bg="hotpink")
 
 #les positions--------------Grid------------#
 
-lbl = Label(root, text=text).pack()
+# lbl = Label(root, text=text).pack()
 
+Entry_name = Entry(root, width=45)
+Entry_name.grid(row=0, column=0)
 
 root.mainloop()
