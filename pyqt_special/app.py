5911831abb89f65d15db468184289ebfce368e1e
vrmelox <akandeabiodoun@gmail.com>
2025-01-04 14:19:23 +0100

    I've created a window and custom the QMainWindow widget

--- pyqt_special/app.py
+++ pyqt_special/app.py
@@ -31,4 +31,27 @@
 # # Start the event loop.
 # app.exec()
 
-#------------------------------testing---------------------
+#------------------------------testing custom window---------------------
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Ma fenêtre")
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
