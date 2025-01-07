f0404e2cb135f3127bedb8a306a430a0c4503c1e
vrmelox <akandeabiodoun@gmail.com>
2025-01-07 14:37:00 +0100

    i've rebuild the game according to poo paradigme

--- pyqt_special/app.py
+++ pyqt_special/app.py
@@ -87,9 +87,37 @@
 # app.exec()
 
 #--------------------testing signals and slots-----------------
+# import sys
+# from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+
+#         self.setWindowTitle("My App")
+
+#         button = QPushButton("Press Me!")
+#         button.setCheckable(True)
+#         button.clicked.connect(self.the_button_was_clicked)
+
+#         # Set the central widget of the Window.
+#         self.setCentralWidget(button)
+
+#     def the_button_was_clicked(self):
+#         print("Clicked!")
+
+
+# app = QApplication(sys.argv)
+
+# window = MainWindow()
+# window.show()
+
+# app.exec()
+
+#------------------second slot which outputs the checkstate-------------
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -100,17 +128,18 @@ class MainWindow(QMainWindow):
         button = QPushButton("Press Me!")
         button.setCheckable(True)
         button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
 
-        # Set the central widget of the Window.
         self.setCentralWidget(button)
 
     def the_button_was_clicked(self):
         print("Clicked!")
 
+    def the_button_was_toggled(self, checked):
+        print("Checked?", checked)
 
 app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
-
 app.exec()
